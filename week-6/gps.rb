5cf1bfffbe300b0bbcac7622a5accc97802bd60e
khamla719 <khamla719@yahoo.com>
2015-12-16 15:42:07 -0600

    Add peer challenge 6.5

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -8,23 +8,15 @@
 
 def serving_size_calc(item_to_make, num_of_ingredients)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
-
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
+    # if item_to_make is not equal to any of the keys in the library, raise arugment error
+    if !library.has_key?(item_to_make)
+          raise ArgumentError.new("#{item_to_make} is not a valid input")
     end
-  end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library.values_at(item_to_make)[0]
+  serving_size = library[item_to_make]
   remaining_ingredients = num_of_ingredients % serving_size
 
-  case remaining_ingredients
-  when 0
+  if remaining_ingredients == 0
     return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
   else
     return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
@@ -38,6 +30,9 @@ p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
+
+
+# Think about where to keep the left-overs. Think about the scope of leftovers, may need to create extra variables line 17
 
 #  Reflection
 
