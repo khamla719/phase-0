d26476f1da0d93b6dd6996e0aec023efe3ac2a4d
khamla719 <khamla719@yahoo.com>
2016-01-03 19:24:17 -0500

    Add challenge 7.2

---
+++ week-7/eloquent.js
@@ -0,0 +1,33 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+var mario = 200;
+mario = mario - 100;
+console.log(mario);
+
+prompt("What's your favorite food?");
+alert("Hey, that's my favorite too!");
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+for (var i = "#"; i.length <= 7; i+="#"){
+  console.log(i);
+}
+
+// Functions
+
+// Complete the `minimum` exercise.
+function returnMin(x, y){
+  return Math.min(x, y);
+}
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+var me= {
+  name: "Khamla",
+  age: "31",
+  favFoods: "Roast Pork, Ramen, Korean BBQ",
+  quirk: "singing absurdly loud in the shower"
+};
